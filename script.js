2c984b91ae9036c5208e045d093a10577cea5229
sabeeha shaik <142984283+sabeeha1234@users.noreply.github.com>
2025-10-28 11:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const allPrices = document.querySelectorAll('.price')
      //function to get sum and append it 
      function getSumAndAppend(){
-        console.log(allPrices)
+       
         // step 1:get total sum
         let totalSum = 0;
         for(let price of allPrices){
